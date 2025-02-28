77b624a5805699a80d431b3e7b56be122a09f388
Your Name <you@example.com>
2025-02-28 18:32:39 +0500

    login redirect added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
     {
         path: "login",
         component: LoginComponent
